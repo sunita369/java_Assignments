bc75b68ff13b23f6c8f14b65fa20e0cf7672ccdc
sunita369 <sunita98mondal@gmail.com>
2021-05-13 22:35:39 +0530

    project 2.2 has been added

---
+++ project 2.2/src/Labour.java
@@ -0,0 +1,12 @@
+
+
+public class Labour extends Employee{
+	public static final double overTime=0.5;
+	public Labour(int employeeId,String employeeName, double salary) {
+		super(employeeId,employeeName,salary);
+	}
+	public double getSalary() {
+		return salary+salary*overTime;
+	}
+
+}
